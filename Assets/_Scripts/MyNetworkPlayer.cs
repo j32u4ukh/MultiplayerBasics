e34158f0a959f0f0d8b3328b03eab5ae3036eb75
j32u4ukh <j32u4ukh@gmail.com>
2022-01-25 10:43:09 +0800

    Lesson 9: Client 端不做判斷，只送出請求，由 Server 端進行判斷。當判斷條件改變時，才不會兩端使用條件不同(若 Client 未更新程式碼)。

--- Assets/_Scripts/MyNetworkPlayer.cs
+++ Assets/_Scripts/MyNetworkPlayer.cs
@@ -12,18 +12,53 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SyncVar(hook = nameof(handleDisplayNameUpdate))] [SerializeField] private string display_name = "Missing Name";
     [SyncVar(hook = nameof(handleDisplayColorUpdate))] [SerializeField] private Color display_color = Color.white;
 
+    #region Server
+    /// <summary>
+    /// Call by server
+    /// </summary>
+    /// <param name="display_name"></param>
     [Server]
     public void setDisplayName(string display_name)
     {
         this.display_name = display_name;
     }
 
+    /// <summary>
+    /// Call by server
+    /// </summary>
+    /// <param name="color"></param>
     [Server]
     public void setDisplayColor(Color color)
     {
         display_color = color;
     }
 
+    /// <summary>
+    /// Call by client
+    /// [Command]: For clients calling a method on the server
+    /// </summary>
+    /// <param name="display_name"></param>
+    [Command]
+    private void cmdSetDisplayName(string display_name)
+    {
+        if (isNameValid(display_name))
+        {
+            rpcLogNewName(display_name);
+            setDisplayName(display_name);
+        }
+        else
+        {
+            Debug.LogWarning($"The new name '{display_name}' is not valid.");
+        }
+    }
+
+    private bool isNameValid(string new_name)
+    {
+        return (2 < new_name.Length) && (new_name.Length < 20);
+    }
+    #endregion
+
+    #region Client
     private void handleDisplayNameUpdate(string origin_name, string new_name)
     {
         display_name_text.text = new_name;
@@ -37,5 +72,24 @@ public class MyNetworkPlayer : NetworkBehaviour
     private void handleDisplayColorUpdate(Color origin_color, Color new_color)
     {
         display_color_renderer.material.SetColor("_BaseColor", new_color);
+    } 
+
+    [ContextMenu("Set My Name")]
+    private void setMyName()
+    {
+        cmdSetDisplayName("M");
     }
+
+    /// <summary>
+    /// [ClientRpc]: For the server calling a method on all clients
+    /// </summary>
+    /// <param name="display_name"></param>
+    [ClientRpc]
+    private void rpcLogNewName(string display_name)
+    {
+        Debug.Log(display_name);
+    }
+
+    // [TargetRpc]: For the server calling a method on a specific client
+    #endregion
 }
