90e284985f93e59e651cd96d0eee93bdf134feeb
j32u4ukh <j32u4ukh@gmail.com>
2022-01-25 08:54:28 +0800

    Lesson 7: SyncVar Callbacks

--- Assets/_Scripts/MyNetworkPlayer.cs
+++ Assets/_Scripts/MyNetworkPlayer.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string display_name = "Missing Name";
-    [SyncVar] [SerializeField] private Color display_color = Color.white;
+    [SerializeField] private TMP_Text display_name_text = null;
+    [SerializeField] private Renderer display_color_renderer = null;
+
+    [SyncVar(hook = nameof(handleDisplayNameUpdate))] [SerializeField] private string display_name = "Missing Name";
+    [SyncVar(hook = nameof(handleDisplayColorUpdate))] [SerializeField] private Color display_color = Color.white;
 
     [Server]
     public void setDisplayName(string display_name)
@@ -18,5 +22,20 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void setDisplayColor(Color color)
     {
         display_color = color;
+    }
+
+    private void handleDisplayNameUpdate(string origin_name, string new_name)
+    {
+        display_name_text.text = new_name;
+    }
+
+    /// <summary>
+    /// 名稱不一定要叫做 origin_color/new_color，但需要新舊兩個參數
+    /// </summary>
+    /// <param name="origin_color">即便原本沒有使用，也需給予此參數</param>
+    /// <param name="new_color"></param>
+    private void handleDisplayColorUpdate(Color origin_color, Color new_color)
+    {
+        display_color_renderer.material.SetColor("_BaseColor", new_color);
     }
 }
